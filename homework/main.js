96d0c22599b5b038cd64fa364db4c0428427b222
arslonbekXX <arslonbek.lion067@gmail.com>
2022-12-26 19:31:07 +0500

    lesson-23 finish-codes

---
+++ homework/main.js
@@ -0,0 +1,23 @@
+const names = [
+	"Abdulloh",
+	"Hafizulloh",
+	"Azizxo'ja",
+	"Fayzullo",
+	"Abdurahmon",
+	"Azizbek",
+	"Mirafzal",
+	"Nilufar",
+	"Muhammad",
+	"Muhammadzohid",
+	"Murodulla",
+	"Sardor",
+	"Azimjon",
+	"Sanjarbek",
+	"Fayzulloh",
+	"Davronali",
+	"Sherzod",
+	"Ziyoviddin",
+	"Murod",
+].sort(() => 0.5 - Math.random());
+
+console.log("names = ", names);
