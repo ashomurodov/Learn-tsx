571abe7457d8b65e694e5a7ae50e537322342117
arslonbekXX <arslonbek.lion067@gmail.com>
2023-01-09 14:49:30 +0500

    lesson-25-examine-2 start-codes

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("🥶 Examine-2 🥶");
+/**
+ *
+ * @param {string} char
+ * @returns {boolean}
+ */
+function isLetter(char) {
+	return char.length === 1 && char.match(/[a-z]/i);
+}
+
+/**
+ *
+ * @param {string} str
+ * @returns {string}
+ */
+
+function find(str) {}
+
+/**
+ * Cases :
+ *
+ * find("a2bc10d!22bc-23avv-2") -> 9
+ * find("bca2-2b2") -> 2
+ * find("dbcc10b100-200b200") -> 110
+ * find("") -> 0
+ * find("-10-10-10a10-20b10-10") -> 40
+ */
