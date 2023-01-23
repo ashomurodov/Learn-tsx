c3b73c51766c373c534fdff8856b34f199381243
arslonbekXX <arslonbek.lion067@gmail.com>
2023-01-23 12:49:48 +0500

    lesson-31 start-codes

--- README.md
+++ README.md
@@ -1,46 +1,37 @@
-# ---------- 20.01.23 ----------(30)
+# ---------- 23.01.23 ----------(31)
 
-## Checking Homeworks 👍🏻
-
-## Values and Variables 👍🏻
-
-## Variable name conventions 👍🏻
-
-## Data Types 👍🏻
-
-## Primitive Data types 👍🏻
-
-     ✅ Number 👍🏻
-     ✅ String 👍🏻
-     ✅ Boolean 👍🏻
-     ✅ Undefined 👍🏻
-     ✅ Null 👍🏻
+## Checking Homeworks
 
 ## let && const && var
 
-     ✅ Hoisting
-     ✅ Temporal dead zone(let, const)
-     ✅ Scoping
-          🔷 Global scope -> full file
-          🔷 Function scope -> function (){}
-          🔷 Block scope -> {} || for(){}
-
-## Basic operators 👍🏻
-
-     ✅ Math operators (%,+,-,++,--,*,/,*=,+=)
-     ✅ Assignment operators (=)
-     ✅ Comparison operators (>, <, >=, <=, ==, ===, !=, !==)
-
-## Coding Challenge 👍🏻
-
-     ✅ CALC BMI(Body Mass Index) BMI = weight / height ** 2
-          🔷 Save values variables: height and wight
-          🔷 calc BMI with formula
-          🔷 create markResult variables
-               🎁 TEST DATA 1: Mark: weight: 78kg, height: 1.69m || Kent : weight: 92kg, height: 1.95m
-               🎁 TEST DATA 2: Mark: weight: 95kg, height: 1.88m || Kent : weight: 85kg, height: 1.76m
-     ✅ swap algorithm
-
 ## Strings and Template Literals
+
+      ✅ Using JS expression
+      ✅ Multi line
+
+## If && Else Statements
+
+## Statement vs Expression
+
+      ✅ https://www.joshwcomeau.com/javascript/statements-vs-expressions/
+      ✅ https://www.youtube.com/watch?v=WVyCrI1cHi8&t=1s
+
+## Type Conversion and Coercion
+
+## Truthy and Falsy values
+
+## Equality Operators == vs ===
+
+## Coding Challenge(1)
+
+## Boolean Logic
+
+      ✅ &&
+      ✅ ||
+      ✅ !
+
+## Logic Operators
+
+## Coding Challenge(2)
 
 ## Coding JS Warmup-1
--- main.js
+++ main.js
@@ -64,7 +64,19 @@
 // console.log(typeof value);
 // ---- Coding Challenge
 
-// CC - 1
+/**
+ * Coding Challenge #1 :
+ * Mark and Kent are trying to compare their BMI (Body Mass Index),
+ * which is calculated using the formula:
+ * BMI = mass / height ** 2
+ *
+ *
+ * Task-1 : Store Mark's and Kent's mass and height in variables
+ * Task-2 : Calculate both their BMIs using the formula (you can even implement both versions)
+ * Task-3 : Create a Boolean variable 'markResult' containing information about whether Mark has a higher BMI than Kent
+ *
+ *
+ */
 // let markWeight;
 // let markHeight;
 // let markBMI;
@@ -91,9 +103,55 @@
 // console.log(a);
 // console.log(b);
 
-let a = 2;
-let b = 4;
-console.log(typeof a);
-console.log(typeof b);
-console.log(a == b);
-console.log(a === b);
+// let a = 2;
+// let b = 4;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(a == b);
+// console.log(a === b);
+
+console.log("Lesson-31");
+
+/**
+ * Coding Challenge #2 :
+ * Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+ *
+ * Task-1 : Print a nice output to the console, saying who has the higher BMI.
+ * The message is either "Mark's BMI is higher than Kent's!" or "Kent's BMI is higher than Mark's!"
+ *
+ * Task-2 : Use a template literal to include the BMI values in the outputs.
+ * Example: "Mark's BMI (28.3) is higher than Kent's (23.9)!"
+ *
+ */
+
+/**
+ * Coding Challenge #3 :
+ *
+ * Gimnastika bo'yicha ikkita jamoa mavjud: Delfinlar(Dolphins) va Koalalar(Koalas).
+ *  Ular bir-birlari bilan 3 marta raqobatlashadilar. Eng yuqori o'rtacha ball(averageScore) to'plagan g'olib kubokni(🏆) qo'lga kiritadi!
+ *
+ *
+ * Task 1 : O'rtacha ballarini hisoblaymiz (averageScore)
+ *
+ * Task 2 : Musobaqa g'olibini aniqlash uchun jamoaning o'rtacha ballarini solishtiring va uni konsolga chop eting.
+ * Shuni unutmangki, durang bo'lishi mumkin, shuning uchun buni ham sinab ko'ring
+ *
+ * Bonus 1 : Minimal 100 ball talabini qo'shing. Ushbu qoidaga ko'ra,
+ * jamoa boshqa jamoadan yuqori ballga ega bo'lsa va ayni paytda kamida 100 ball to'plagan taqdirdagina g'alaba qozonadi
+ *
+ * Bonus 2 : Minimal ball durang uchun ham amal qiladi!
+ * Shunday qilib, durang faqat ikkala jamoa bir xil ochkoga ega bo'lsa va
+ * ikkalasi ham 100 ochkodan katta yoki teng ballga ega bo'lganda sodir bo'ladi.
+ * Aks holda, hech bir jamoa kubokni qo'lga kirita olmaydi
+ *
+ *
+ * Test Data :
+ *    ✅ Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+ *    ✅ Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+ *    ✅  Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+ *
+ * Result template:
+ *      Winner : {teamName} winner 🏆
+ *      durrang uchung : {teamName1} and {teamName2} winners 🏆
+ *      xech kim yutmaganda : No winners 🥶
+ */
