f8b19af55a311cb581e8eb16f44e7b0120c3a249
arslonbekXX <arslonbek.lion067@gmail.com>
2023-05-16 08:56:51 +0500

    lesson-75 finish-codes

--- dist/github/index.js
+++ dist/github/index.js
@@ -14,12 +14,42 @@ function getRepositories(username, cb) {
     const repos = constants_1.repositories.filter((r) => r.owner === username);
     setTimeout(() => cb(repos), constants_1.TIME);
 }
+function getBranches(repoID, cb) {
+    console.log("[BRANCHES][🌀] Loading...");
+    const _branches = constants_1.branches.filter((b) => b.repoID === repoID);
+    setTimeout(() => cb(_branches), constants_1.TIME);
+}
+function getCommits(branchID, cb) {
+    console.log("[COMMITS][🌀] Loading...");
+    const _commits = constants_1.commits.filter((c) => c.branchID === branchID);
+    setTimeout(() => cb(_commits), constants_1.TIME);
+}
 /** DISPLAY FUNCTIONS */
+const displayCommits = (commits) => {
+    console.log("commits = ", commits);
+};
+const displayBranches = (branches) => {
+    console.log("branches = ", branches);
+    getCommits(branches[0].id, displayCommits);
+};
 const displayRepositories = (repos) => {
     console.log("repos = ", repos);
+    getBranches(repos[0].id, displayBranches);
 };
 const displayUser = (user) => {
     console.log("user = ", user);
     getRepositories(user.username, displayRepositories);
 };
 getUser("ars@domain.com", displayUser);
+getUser("ars@domain.com", (user) => {
+    console.log("user = ", user);
+    getRepositories(user.username, (repos) => {
+        console.log("repos = ", repos);
+        getBranches(repos[0].id, (branches) => {
+            console.log("branches = ", branches);
+            getCommits(branches[0].id, (commits) => {
+                console.log("commits = ", commits);
+            });
+        });
+    });
+});
--- src/github/index.ts
+++ src/github/index.ts
@@ -1,4 +1,4 @@
-import { TIME, users, repositories } from "./constants";
+import { TIME, users, repositories, branches, commits } from "./constants";
 import { IEntity, IFunction } from "./types";
 
 /** BACKEND FUNCTIONS */
@@ -18,14 +18,52 @@ function getRepositories(username: string, cb: IFunction.Callback<IEntity.Repo[]
 	setTimeout(() => cb(repos), TIME);
 }
 
+function getBranches(repoID: number, cb: IFunction.Callback<IEntity.Branch[]>) {
+	console.log("[BRANCHES][🌀] Loading...");
+	const _branches = branches.filter((b) => b.repoID === repoID);
+
+	setTimeout(() => cb(_branches), TIME);
+}
+
+function getCommits(branchID: number, cb: IFunction.Callback<IEntity.Commit[]>) {
+	console.log("[COMMITS][🌀] Loading...");
+	const _commits = commits.filter((c) => c.branchID === branchID);
+
+	setTimeout(() => cb(_commits), TIME);
+}
+
 /** DISPLAY FUNCTIONS */
+
+const displayCommits = (commits: IEntity.Commit[]) => {
+	console.log("commits = ", commits);
+};
+
+const displayBranches = (branches: IEntity.Branch[]) => {
+	console.log("branches = ", branches);
+	getCommits(branches[0].id, displayCommits);
+};
 
 const displayRepositories = (repos: IEntity.Repo[]) => {
 	console.log("repos = ", repos);
+	getBranches(repos[0].id, displayBranches);
 };
 
 const displayUser = (user: IEntity.User) => {
 	console.log("user = ", user);
 	getRepositories(user.username, displayRepositories);
 };
+
 getUser("ars@domain.com", displayUser);
+
+getUser("ars@domain.com", (user) => {
+	console.log("user = ", user);
+	getRepositories(user.username, (repos: IEntity.Repo[]) => {
+		console.log("repos = ", repos);
+		getBranches(repos[0].id, (branches: IEntity.Branch[]) => {
+			console.log("branches = ", branches);
+			getCommits(branches[0].id, (commits: IEntity.Commit[]) => {
+				console.log("commits = ", commits);
+			});
+		});
+	});
+});
