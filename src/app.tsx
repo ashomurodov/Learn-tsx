5e20f63e86eaa74df9ddf4a9be253ca6bed37452
arslonbekXX <arslonbek.lion067@gmail.com>
2023-06-19 18:38:27 +0500

    lesson-90 finish-codes

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
-import { Counter } from "./components";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
 export interface ICount {
 	id: number;
 	value: number;
 }
-interface AppProps {}
-interface AppState {
+export interface AppProps {}
+export interface AppState {
 	counters: ICount[];
 }
+
+export enum TYPE {
+	INCREMENT,
+	DECREMENT,
+	DELETE,
+}
+
 export default class App extends Component<AppProps, AppState> {
 	state = {
 		counters: [
-			{ id: 1, value: 10 },
-			{ id: 2, value: 20 },
+			{ id: 1, value: 1 },
+			{ id: 2, value: 2 },
 			{ id: 3, value: 0 },
-			{ id: 4, value: 5 },
+			{ id: 4, value: 3 },
 		],
 	};
 
@@ -25,22 +33,37 @@ export default class App extends Component<AppProps, AppState> {
 		}));
 	};
 
-	handleIncrement = (countID: number, step = 10) => {
-		console.log("countID = ", countID);
-		// this.setState((prev) => ({ counters: prev.counters.map((count) => count + step) }));
+	handleAction = (countID: number, actionType: TYPE) => {
+		switch (actionType) {
+			case TYPE.DELETE: {
+				this.setState((prev) => ({
+					counters: prev.counters.filter((count) => count.id !== countID),
+				}));
+				break;
+			}
+			default: {
+				const counters = [...this.state.counters];
+				const countIdx = counters.findIndex((count) => count.id === countID);
+				const count = counters[countIdx];
+				if (actionType === TYPE.DECREMENT && count.value <= 0) return;
+				count.value += actionType === TYPE.INCREMENT ? 1 : -1;
+
+				this.setState({ counters });
+			}
+		}
 	};
 
 	render() {
 		const { counters } = this.state;
 		return (
-			<main className="container">
-				<button className="my-2 btn btn-primary" onClick={this.handleReset}>
-					Reset
-				</button>
-				{counters.map((count, idx) => (
-					<Counter key={idx} count={count} onIncrement={this.handleIncrement} />
-				))}
-			</main>
+			<div>
+				<NavBar amount={counters.filter((count) => count.value > 0).length} />
+				<Counters
+					onAction={this.handleAction}
+					onReset={this.handleReset}
+					counters={this.state.counters}
+				/>
+			</div>
 		);
 	}
 }
