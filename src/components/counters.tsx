d98ebd0ae7abae739537066d932976c5b12d0b68
Ayyub <ayyuubshomurodov@gmail.com>
2023-06-24 16:10:28 +0500

    finish

--- src/components/counters.tsx
+++ src/components/counters.tsx
@@ -1,7 +1,25 @@
 import { Component } from "react";
+import Counter from "./counter";
+import { ICount, TYPE } from "../app";
 
-export default class Counters extends Component {
+export interface CountersProps {
+  counters: ICount[];
+  onReset: any;
+  onAction: (actionType: TYPE, id: number) => void;
+}
+
+export default class Counters extends Component<CountersProps> {
   render() {
-    return <div>Counters</div>;
+    const { counters, onReset, onAction } = this.props;
+    return (
+      <div className="container mt-4 d-flex flex-column gap-2">
+        <button onClick={onReset} className="btn btn-primary">
+          Reset
+        </button>
+        {counters.map((count) => (
+          <Counter onAction={onAction} count={count} />
+        ))}
+      </div>
+    );
   }
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,21 @@
 import { Component } from "react";
 
-export default class Navbar extends Component {
+interface NavbarProps {
+  amout: number;
+}
+
+export default class Navbar extends Component<NavbarProps> {
   render() {
-    return <div>Navbar</div>;
+    const { amout } = this.props;
+    return (
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="index.html">
+            Navbar
+          </a>
+          <span className="badge text-bg-dark">{amout}</span>
+        </nav>
+      </div>
+    );
   }
 }
