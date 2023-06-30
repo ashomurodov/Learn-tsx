4c913a85ffed3b5066404112f6456a815350a79f
arslonbekXX <arslonbek.lion067@gmail.com>
2023-06-30 18:07:23 +0500

    lesson-93 finish-codes

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,21 @@ import { Navbar, Posts, Comments } from "components";
 interface AppState {
 	postID: number | null;
 }
+
 export default class App extends Component<{}, AppState> {
 	state: AppState = {
 		postID: null,
 	};
 
 	render() {
+		const { postID } = this.state;
 		return (
 			<>
 				<Navbar />
 				<main className="container my-4">
-					<Comments postID={this.state.postID} />
+					{postID && (
+						<Comments onExit={() => this.setState({ postID: null })} postID={this.state.postID} />
+					)}
 					<Posts onView={(postID) => this.setState({ postID })} />
 				</main>
 			</>
--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Component } from "react";
 
 interface Comment {
@@ -10,6 +11,7 @@ interface Comment {
 
 interface CommentsProps {
 	postID: number | null;
+	onExit: () => void;
 }
 
 interface CommentsState {
@@ -17,10 +19,76 @@ interface CommentsState {
 	isLoading: boolean;
 }
 
-export default class Comments extends Component<CommentsProps, CommentsState> {
-	render() {
-		const { postID } = this.props;
+const baseURL = "https://jsonplaceholder.typicode.com";
 
-		return <div>postID = {this.props.postID}</div>;
+export default class Comments extends Component<CommentsProps, CommentsState> {
+	state: CommentsState = {
+		comments: [],
+		isLoading: true,
+	};
+
+	getComments = async () => {
+		this.setState({ isLoading: true });
+		const { data: comments } = await axios(`${baseURL}/comments?postId=${this.props.postID}`, {
+			headers: {
+				"x-auth-token": "your-token",
+			},
+		});
+
+		this.setState({ comments, isLoading: false });
+	};
+
+	async componentDidMount() {
+		this.getComments();
+	}
+
+	async componentDidUpdate(prevProps: Readonly<CommentsProps>, prevState: Readonly<CommentsState>) {
+		if (prevProps.postID !== this.props.postID) this.getComments();
+	}
+
+	render() {
+		const { postID, onExit } = this.props;
+		const { isLoading, comments } = this.state;
+
+		if (isLoading)
+			return (
+				<div className="d-flex justify-content-center">
+					<div className="spinner-border">
+						<span className="visually-hidden">Loading...</span>
+					</div>
+				</div>
+			);
+
+		return (
+			<div>
+				<h1 className="text-center">
+					Post-{postID} (
+					<button className="btn btn-danger" onClick={onExit}>
+						Exit
+					</button>
+					)
+				</h1>
+				<table className="table table-dark table-striped table-bordered">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>name</th>
+							<th>body</th>
+							<th>email</th>
+						</tr>
+					</thead>
+					<tbody>
+						{comments.map(({ id, name, body, email }) => (
+							<tr key={id}>
+								<td>{id}</td>
+								<td>{name}</td>
+								<td>{body}</td>
+								<td>{email}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		);
 	}
 }
