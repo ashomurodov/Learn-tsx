c90395c1b3b1a1b19cdf8020bc8c5596588028e1
Ayyub <ayyuubshomurodov@gmail.com>
2023-07-20 18:05:32 +0500

    fed

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,5 @@
-import React, { Component } from "react";
-import Home from "./pages/home";
-import Item from "./pages/item";
+interface AppProps {}
 
-interface AppState {
-  token: string;
-}
+const App = (props: AppProps) => <h1>App Page</h1>;
 
-export default class App extends Component<{}, AppState> {
-  state: AppState = {
-    token: "",
-  };
-
-  addToken = (token: string) => {
-    this.setState({ token });
-    localStorage.setItem("token", token);
-  };
-
-  deleteToken = () => {
-    localStorage.removeItem("token");
-    this.setState({ token: "" });
-  };
-
-  componentDidMount() {
-    const token = localStorage.getItem("token") ? localStorage.getItem("token") : "";
-    if (token?.length) this.setState({ token });
-  }
-  render() {
-    const { token } = this.state;
-
-    if (token)
-      return (
-        <div className="App d-flex justify-content-center">
-          <Item token={token} deleteToken={this.deleteToken} />
-        </div>
-      );
-
-    return (
-      <div className="App d-flex justify-content-center">
-        <Home addToken={this.addToken} />
-      </div>
-    );
-  }
-}
+export default App;
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Categories } from "./categories";
-export { default as Products } from "./products";
-export { default as Gallery } from "./gallery";
-export { default as List } from "./list";
