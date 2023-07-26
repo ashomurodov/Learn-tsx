e304aaac7e43e9d2b92b9f8ab13154e85d65fbf9
Ayyub <ayyuubshomurodov@gmail.com>
2023-07-26 12:59:23 +0500

    hr

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { Toaster } from "react-hot-toast";
 import App from "./app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "assets/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-	<>
-		<App />
-		<Toaster />
-	</>
+  <>
+    <App />
+  </>
 );
